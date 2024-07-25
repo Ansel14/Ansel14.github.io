9d7c58fc62e2004f94b537c12b6f205536e45a37
Ansel14 <jake.gamaro@gmail.com>
2024-07-25 13:21:17 +0800

    jake commit

---
+++ dist/js/slider.js
@@ -0,0 +1,11 @@
+var slider = tns({
+  container: ".project__slider",
+  items: 1,
+  speed: 300,
+  controls: true,
+  controlsContainer: "#controls",
+  controlsPosition: "bottom",
+  prevButton: ".prev",
+  nextButton: ".next",
+  slideBy: "page",
+});
