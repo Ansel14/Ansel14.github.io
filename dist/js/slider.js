99dd51685f5dcd33ff62d0fc0969c05b5838608f
Ansel14 <jake.gamaro@gmail.com>
2024-08-06 11:00:04 +0800

    project section fixed

--- dist/js/slider.js
+++ dist/js/slider.js
@@ -1,7 +1,9 @@
 var slider = tns({
   container: ".project__slider",
+  mode: "carousel",
   items: 1,
   speed: 300,
+  gutter: 200,
   controls: true,
   controlsContainer: "#controls",
   controlsPosition: "bottom",
